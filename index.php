eee545f8e6a283031e5d31a6330fa0883090fe2a
Sven Bingert <sven.bingert@gwdg.de>
2016-07-01 16:43:25 +0200

    added first files

---
+++ index.php
@@ -0,0 +1,90 @@
+<?php
+header('Content-Type: text/xml;charset=UTF-8');
+
+$SESSION_STORAGE = './storage/actual.txt';
+if (!isset($_GET['verb'])) {
+  if (isset($_GET['cmd'])) {
+    $cmd = $_GET['cmd'];
+    switch ($cmd) {
+      case 'set':
+        $value = $_GET['value'];
+        file_put_contents($SESSION_STORAGE, $value);
+        print '<success/>';
+        break;
+
+      case 'get':
+        print file_get_contents($SESSION_STORAGE);
+        break;
+
+      default:
+        print '<failure/>';
+        break;
+    }
+  }
+  else {
+    print '<failure/>';
+  }
+  return;
+}
+$idArray = explode('&',$_SERVER["QUERY_STRING"]);
+foreach ($idArray as $index => $avPair)
+{
+  list($ignore, $value) = explode("=", $avPair);
+  $id[$index] = $value;
+}
+//$verb = $_GET['verb'];
+
+$verb = $id[0];
+switch($verb) {
+case 'Identify' :
+    if (count($idArray) == 1){
+        //readfile('responses/standard/identify.xml');
+        include 'responses/standard/identify.php';
+    } else {
+        readfile('responses/standard/error_badargument.xml');
+    }
+    break;
+    
+case 'ListSets' :
+    readfile('responses/standard/list_sets.xml');
+    break;
+    
+case 'ListMetadataFormats' :
+    readfile('responses/standard/list_metadata_formats.xml');
+    break;
+    
+case 'ListIdentifiers' :
+    readfile('responses/standard/list_identifiers.xml');
+    break;
+
+case 'ListRecords' :
+    if (isset($_GET['set'])) {
+        $set = $_GET['set'];
+    }
+    elseif (file_exists($SESSION_STORAGE)) {
+        $set = file_get_contents($SESSION_STORAGE);
+    }
+    else {
+        $set = 'good';
+    }
+
+    if ($set == 'good') {
+        readfile('responses/list_records_good.xml');
+    }
+    else if ($set == 'deleted') {
+        readfile('responses/list_records_deleted.xml');
+    }
+    else if ($set == '10rec') {
+        readfile('responses/list_records_10rec-step2.xml');
+    }
+    else if ($set == '10rec2') {
+        readfile('responses/10-test-phase2-02.xml');
+    }
+    else if ($set == 'accents') {
+        readfile('responses/accents.xml');
+    }
+    break;
+default:
+    readfile('responses/standard/error.xml');
+    break;
+}
---
+++ responses/standard/identify.php
@@ -0,0 +1,89 @@
+<?php
+
+// Set the content type to be XML, so that the browser will   recognise it as XML.
+header( "content-type: application/xml; charset=ISO-8859-15" );
+
+// "Create" the document.
+$xml = new DOMDocument( "1.0", "UTF-8" );
+
+// 
+// LEVEL 1 OAI_PMH
+//
+$xml_OAI_PMH = $xml->createElement( "OAI-PMH" );
+$xml_OAI_PMH->setAttribute( "xmlns", "http://www.openarchives.org/OAI/2.0/" );
+$xml_OAI_PMH->setAttribute( "xmlns:xsi", "http://www.w3.org/2001/XMLSchema-instance" );
+$xml_OAI_PMH->setAttribute( "xsi:schemaLocation" , "http://www.openarchives.org/OAI/2.0/ http://www.openarchives.org/OAI/2.0/OAI-PMH.xsd");
+    
+$xml_responseDate = $xml->createElement( "responseDate", date('c') );
+
+$xml_request = $xml->createElement( "request", "http://humanities-data-centre.de/wp-content/uploads/oai2" );
+$xml_request->setAttribute( "verb", "Identify" );
+
+//
+// LEVEL 2 IDENTIFY
+//
+  $xml_identify = $xml->createElement( "Identify" );
+
+  $xml_repositoryName = $xml->createElement( "repositoryName" , "Humanities Data Centre - Application Conservation" );
+  $xml_identify->appendChild( $xml_repositoryName );
+
+  $xml_baseURL = $xml->createElement( "baseURL" ,"http://humanities-data-centre.de/wp-content/uploads/oai2" );
+  $xml_identify->appendChild( $xml_baseURL );
+
+  $xml_protocolVersion = $xml->createElement( "protocolVersion" , "2.0" );
+  $xml_identify->appendChild( $xml_protocolVersion );
+
+  $xml_adminEmail = $xml->createElement("adminEmail" , "sbinger1@gwdg.de");
+  $xml_identify->appendChild( $xml_adminEmail );
+
+  $xml_adminEmail = $xml->createElement("adminEmail" , "hdc-portal-admin@gwdg.de");
+  $xml_identify->appendChild( $xml_adminEmail );
+
+  $xml_earliestDatestamp = $xml->createElement("earliestDatestamp" , "2016-01-01T12:00:00Z");
+  $xml_identify->appendChild( $xml_earliestDatestamp );
+
+  $xml_deletedRecord = $xml->createElement("deletedRecord" , "transient");
+  $xml_identify->appendChild( $xml_deletedRecord );
+
+  $xml_granularity = $xml->createElement("granularity" , "YYYY-MM-DDThh:mm:ssZ");
+  $xml_identify->appendChild( $xml_granularity );
+
+  $xml_compression = $xml->createElement("compression" , "deflate");
+  $xml_identify->appendChild( $xml_compression );
+//
+// LEVEL 3 DESCRIPTION
+//
+    $xml_description = $xml->createElement("description");
+//
+//  LEVEL 4 OAI-IDENTIFIER
+//
+      $xml_oai_identifier = $xml->createElement("oai-identifier");
+      $xml_oai_identifier->setAttribute( "xmlns", "http://www.openarchives.org/OAI/2.0/oai-identifier" );
+      $xml_oai_identifier->setAttribute( "xmlns:xsi", "http://www.w3.org/2001/XMLSchema-instance" );
+      $xml_oai_identifier->setAttribute( "xsi:schemaLocation" , "http://www.openarchives.org/OAI/2.0/oai-identifier http://www.openarchives.org/OAI/2.0/oai-identifier.xsd" );
+
+      $xml_scheme = $xml->createElement("scheme" ,"oai" );
+      $xml_oai_identifier->appendChild( $xml_scheme );
+
+      $xml_repositoryIdentifier = $xml->createElement("repositoryIdentifier" , "humanities-data-centre.de");
+      $xml_oai_identifier->appendChild( $xml_repositoryIdentifier );
+
+      $xml_delimiter = $xml->createElement("delimiter" , ":"); 
+      $xml_oai_identifier->appendChild( $xml_delimiter );
+
+      $xml_sampleIdentifier = $xml->createElement("sampleIdentifier" , "oai:humanities-data-centre.de:loc.music/musdi.002"); 
+      $xml_oai_identifier->appendChild( $xml_sampleIdentifier );
+
+      $xml_description->appendChild( $xml_oai_identifier );
+//
+// END LEVEL 4
+    $xml_identify->appendChild( $xml_description );
+// END LEVEL 3 
+  $xml_OAI_PMH->appendChild( $xml_responseDate );
+  $xml_OAI_PMH->appendChild( $xml_request );
+  $xml_OAI_PMH->appendChild( $xml_identify );
+// END LEVEL 2
+$xml->appendChild( $xml_OAI_PMH );
+
+echo $xml->saveXML();
+?>
