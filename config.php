cb47989ecfb785a5ac66a272ad529bb5d1497228
Sven Bingert <sven.bingert@gwdg.de>
2016-07-02 11:10:38 +0200

    more entries moved to config

--- config.php
+++ config.php
@@ -2,6 +2,7 @@
 
 $config_baseURL = "http://humanities-data-centre.de/wp-content/uploads/oai2";
 $config_repositoryName = "Humanities Data Centre - Application Conservation";
+
 $config_adminEmail1 = "sbinger1@gwdg.de";
 $config_adminEmail2 = "hdc-portal-admin@gwdg.de";
 
