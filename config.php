344a8fa9e43ce68ed0fc3456ab1c97eefda50a17
Sven Bingert <sven.bingert@gwdg.de>
2016-07-05 12:01:06 +0200

    minor

--- config.php
+++ config.php
@@ -6,6 +6,4 @@ $config_repositoryName = "Humanities Data Centre - Application Conservation";
 $config_adminEmail1 = "sbinger1@gwdg.de";
 $config_adminEmail2 = "hdc-portal-admin@gwdg.de";
 
-    
-    
 ?>
